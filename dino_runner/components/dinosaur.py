ebc9c9d0e1ca8f4f59e0b0de8914c66d496214c0
KAREL <canseto@gmail.com>
2022-10-05 16:32:34 -0400

    clase_1: homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,7 +1,7 @@
 import pygame 
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur(Sprite):
     X_POS = 80
@@ -16,24 +16,36 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.dino_jump_vel = self.JUMP_VEL
 
-    def update(self, user_input):
+    def verify(self):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
+
+    def update(self, user_input):
+        self.verify()
         
-        if (user_input [pygame.K_UP] or user_input[pygame.K_SPACE]) and not self.dino_jump:
+        if user_input [pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_jump = False
+            self.dino_run = False
+        elif (user_input [pygame. K_UP] or user_input[pygame.K_SPACE]) and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         elif not self.dino_jump:
-            self.dino_jump = False
             self.dino_run = True
+            self.dino_jump = False
+            self.dino_duck = False
 
         if self.step_index >= 10:
             self.step_index = 0
-    
+
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect = self.image.get_rect()
@@ -51,6 +63,13 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.dino_jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = 350
+        self.step_index += 1
 
 
     def draw(self, screen: pygame.Surface):
